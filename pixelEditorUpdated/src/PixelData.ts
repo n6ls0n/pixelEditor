6eb13a88239ed800e9a3b7c7a3e0005c92b3919a
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-08-01 08:51:45 -0400

    Updated file

--- pixelEditorUpdated/src/PixelData.ts
+++ pixelEditorUpdated/src/PixelData.ts
@@ -1,4 +1,4 @@
-import LWWMap from "./LWWWMap";
+import LWWMap from "./LWWMap";
 
 export type RGB = [red: number, green: number, blue:number];
 
@@ -8,6 +8,40 @@ export default class PixelData {
 
     constructor (id: string = crypto.randomUUID()){
         this.id = id;
-        this.#data = newLWWMap(this.id, {})
+        this.#data = new LWWMap(this.id, {})
+    }
+
+
+    static key(x: number, y: number){
+        return `${x},${y}`;
+    }
+
+    get value(){
+        return this.#data.value;
+    }
+
+    get state(){
+        return this.#data.state;
+    }
+
+    set(x: number, y:number, value: RGB){
+        const key = PixelData.key(x,y);
+        this.#data.set(key, value);
+    }
+
+    get(x:number, y:number): RGB{
+        const key = PixelData.key(x,y);
+
+        const register = this.#data.get(key);
+        return register ??  [255,255,255]
+    }
+
+    delete(x: number, y:number){
+        const key = PixelData.key(x,y);
+        this.#data.delete(key);
+    }
+
+    merge(state: PixelData["state"]){
+    this.#data.merge(state);
     }
 }
