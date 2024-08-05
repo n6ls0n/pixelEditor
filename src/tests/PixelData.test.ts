187245fbff73603d6e553a01897b0c45eeccfaa3
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-08-05 11:20:00 -0400

    Updated test suite

--- src/tests/PixelData.test.ts
+++ src/tests/PixelData.test.ts
@@ -1,1 +1,22 @@
 import {describe, expect, test} from '@jest/globals';
+import PixelData from '../PixelData';
+import LWWMap from '../LWWMap';
+import { RGB } from '../types';
+
+describe('PixelData', () => {
+
+    test('new PixelData instance', () => {
+      const id = 'customId';
+      const pixelData = new PixelData(id);
+      expect(pixelData.id).toBe(id);
+    });
+
+    test('key', () => {
+      const x = 1;
+      const y = 2;
+      const key = PixelData.key(x, y);
+      expect(key).toBe(`${x},${y}`);
+    });
+
+  // While you could write tests for the remaining methods, the observant reader will notice that they are all wrappers for LWWMap methods. Hence, they are not tested here.
+ });
